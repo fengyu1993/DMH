c1b32964587933ecdc54cb614485c9b790c00b63
Yuhan Chen <54525972+fengyu1993@users.noreply.github.com>
2025-11-03 14:10:47 +0800

    Update InverseKinematics.cpp

--- C++/Inverse_Kinematics/InverseKinematics.cpp
+++ C++/Inverse_Kinematics/InverseKinematics.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Yuhan Chen (chenyuhan19930920@163.com)
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+*/
+
 #include "InverseKinematics.h"
 #include <iostream>
 
